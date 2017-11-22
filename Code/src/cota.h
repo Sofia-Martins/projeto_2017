195b95a1d6b38eb0e65958e66f6d6988634735f2
Sofia-Martins <sofia.cardoso.martins@gmail.com>
2017-11-22 22:08:13 +0000

    Doxygen: classe cota

--- Code/src/cota.h
+++ Code/src/cota.h
@@ -15,15 +15,33 @@ private:
 	unsigned int atraso;
 
 public:
-	//construtor
+	/**
+	 * @brief Construtor da classe Cota
+	 * @param emDia True se a cota do associado está em dia, False em caso contrário
+	 * @param atraso Atraso (em anos) de pagamento da cota. 0 se a cota está em dia.
+	 */
 	Cota(bool emDia,unsigned int atraso);
-
-	//metodos set
+	/**
+	 * @brief Atribui valor ao atributo emDia
+	 * @param emDia True se a cota do associado está em dia, False em caso contrário
+	 * @return void
+	 */
 	void setEmDia(bool emDia);
+	/**
+	 * @brief Atribui valor ao atributo atraso
+	 * @param atraso Atraso (em anos) de pagamento da cota.
+	 * @return void
+	 */
 	void setAtraso(unsigned int atraso);
-
-	//metodos get
+	/**
+	 * @brief Retorna valor do atributo emDia
+	 * @return Retorna True se a cota está em dia e False em caso contrário
+	 */
 	bool getEmDia() const;
+	/**
+	 * @brief Retorna valor do atributo atraso
+	 * @return Retorna o atraso (em anos) de pagamento da cota.
+	 */
 	unsigned int getAtraso() const;
 
 };
