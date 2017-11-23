41037a17fd9c1e9dc128a636d78463d3db487600
LuAF <luisamfreire@hotmail.com>
2017-11-23 11:17:14 +0000

    Doxygen para as classes gestor e eventos

--- Code/src/evento.h
+++ Code/src/evento.h
@@ -29,35 +29,117 @@ private:
 
 public:
 	// Construtor
+	/**
+	 * @brief Construtor com parâmetros
+	 * @param planeadores Vetor com os planeadores do evento
+	 * @param organizadores Vetor com os organizadores do evento
+	 * @param local Especifica local do evento
+	 * @param tema Especifica tema do evento
+	 * @param data Especifica data do evento
+	 * @param apoioEvento Especifica se o evento e apoiado ou nao
+	 */
 	Evento(std::vector <unsigned int> planeadores, std::vector<unsigned int> organizadores, std::string local, std::string tema, Data data, Apoio apoioEvento);
 
 	// Metodos get
-
+	/**
+	 * @brief Retorna valor do atributo planeadores
+	 * @return Valor do atributo planeadores
+	 */
 	std::vector<unsigned int> getPlaneadores() const;
+	/**
+	 * @brief Retorna valor do atributo organizadores
+	 * @return Valor do atributo organizadores
+	 */
 	std::vector<unsigned int> getOrganizadores() const;
+	/**
+	 * @brief Retorna valor do atributo local
+	 * @return Valor do atributo local
+	 */
 	std::string getLocal() const;
+	/**
+	 * @brief Retorna valor do atributo tema
+	 * @return Valor do atributo tema
+	 */
 	std::string getTema() const;
+	/**
+	 * @brief Retorna valor do atributo data
+	 * @return Valor do atributo data
+	 */
 	Data getData() const;
+	/**
+	 * @brief Funcao virtual para o retorno dos atributo formadores, na sub class escola de verao
+	 * @return Valor do atributo nomeSubArea
+	 */
 	virtual std::vector<unsigned int> getFormadores() const { return {}; }
+	/**
+	 * @brief Retorna valor do atributo apoioEvento
+	 * @return Valor do atributo apoioEvento
+	 */
 	Apoio getApoioEvento() const;
 
 
 
 	// Metodos set
-
+	/**
+	 * @brief Atribui valor do argumento ao atributo local
+	 * @param local Valor que será atribuído ao atributo local
+	 * @return void
+	 */
 	void setLocal(std::string local);
+	/**
+	 * @brief Atribui valor do argumento ao atributo tema
+	 * @param tema Valor que será atribuído ao atributo tema
+	 * @return void
+	 */
 	void setTema(std::string tema);
+	/**
+	 * @brief Atribui valor do argumento ao atributo apoioEvento
+	 * @param apoio Valor que será atribuído ao atributo apoioEvento
+	 * @return void
+	 */
 	void setApoio(Apoio apoio);
 
 	// Outros metodos
 
+	/**
+	 * @brief Funcao virtual para imprimir cada um dos tipos de eventos
+	 */
 	virtual void show() const;
+	/**
+	 * @brief Adiciona o planeador especificado ao atributo planeadores
+	 * @param planeador Especifica planeador a adicionar
+	 * @return void
+	 */
 	void addPlaneador(unsigned int planeador);
+	/**
+	 * @brief Adiciona o organizador especificado ao atributo organizadores
+	 * @param organizador Especifica organizador a adicionar
+	 * @return void
+	 */
 	void addOrganizador(unsigned int organizador);
+	/**
+	 * @brief Remove o planeador especificado do atributo planeadores
+	 * @param planeador Especifica planeador a remover
+	 * @return void
+	 */
 	void removePlaneador(unsigned int planeador);
+	/**
+	 * @brief Remove o organizador especificado do atributo organizadores
+	 * @param organizador Especifica organizador a remover
+	 * @return void
+	 */
 	void removeOrganizador(unsigned int organizador);
+	/**
+	 * @brief Funcao virtual para obter numero de participantes de cada tipo de evento
+	 */
 	virtual unsigned int getNumeroParticipantes() const { return 0; }
+	/**
+	 * @brief Funcao virtual para remover formador da sub class escola de verao
+	 */
 	virtual void removeFormador(unsigned int formador) {}
+	/**
+	 * @brief Funcao virtual para verificar o tipo de evento
+	 */
 	virtual bool escolaVerao() { return false;}
 
 };
@@ -73,16 +155,41 @@ private:
 
 public:
 	// Construtor
+	/**
+	 * @brief Construtor com parâmetros
+	 * @param planeadores Vetor com os planeadores da conferencia
+	 * @param organizadores Vetor com os organizadores da conferencia
+	 * @param local Especifica local da conferencia
+	 * @param tema Especifica tema da conferencia
+	 * @param data Especifica data da conferencia
+	 * @param apoioEvento Especifica se a conferencia e apoiado ou nao
+	 * @param numeroParticipantes Especifica o numero de participantes
+	 */
 	Conferencia(std::vector<unsigned int> planeadores, std::vector<unsigned int> organizadores, std::string local, std::string tema, Data data, Apoio apoioEvento, unsigned int numeroParticipantes);
 
 	// Metodos get
+	/**
+	 * @brief Retorna valor do atributo numeroParticipantes
+	 * @return Valor do atributo numeroParticipantes
+	 */
 	unsigned int getNumeroParticipantes() const;
 
 	// Metodos set
+	/**
+	 * @brief Atribui valor ao atributo numeroParticipantes
+	 * @param numeroParticipantes Especifica o valor do atributo numeroParticipantes
+	 */
 	void setNumeroParticipantes(unsigned int numeroParticipantes);
 
 	//outros metodos
+	/**
+	 * @brief Imprime a conferencia na consola
+	 */
 	void show() const;
+	/**
+	 * @brief Retorna false pois nao e uma escola de verao
+	 * @return false
+	 */
 	bool escolaVerao() { return false; }
 };
 
@@ -97,6 +204,16 @@ private:
 
 public:
 	//Construtor
+	/**
+	 * @brief Construtor com parâmetros
+	 * @param planeadores Vetor com os planeadores da conferencia
+	 * @param organizadores Vetor com os organizadores da conferencia
+	 * @param local Especifica local da conferencia
+	 * @param tema Especifica tema da conferencia
+	 * @param data Especifica data da conferencia
+	 * @param apoioEvento Especifica se a conferencia e apoiado ou nao
+	 * @param formadores Vetor com os formadores da escola de verao
+	 */
 	EscolaVerao(std::vector <unsigned int> planeadores,
 		std::vector<unsigned int> organizadores,
 		std::string local,
@@ -106,12 +223,33 @@ public:
 		std::vector<unsigned int> formadores);
 
 	// Metodos get
+	/**
+	 * @brief Retorna valor do atributo formadores
+	 * @return Valor do atributo formadores
+	 */
 	std::vector<unsigned int> getFormadores() const;
 
 	// Outros metodos
+	/**
+	 * @brief Imprime a escola de verao na consola
+	 */
 	void show() const;
+	/**
+	 * @brief Adiciona o formador especificado ao atributo formadores
+	 * @param formador Especifica formador a adicionar
+	 * @return void
+	 */
 	void addFormador(unsigned int formador);
+	/**
+	 * @brief Remove o formador especificado do atributo formadores
+	 * @param formador Especifica formador a remover
+	 * @return void
+	 */
 	void removeFormador(unsigned int formador);
+	/**
+	 * @brief Retorna true pois e uma escola de verao
+	 * @return true
+	 */
 	bool escolaVerao() { return true; }
 };
 
--- Code/src/gestor.h
+++ Code/src/gestor.h
@@ -27,28 +27,109 @@ private:
 	std::vector<Email*> emailsEnviados;
 public:
 	//construtor
+	/**
+		 * @brief Le o gestor do ficheiro de gestores
+		 * @param nome Especifica nome do gestor
+		 * @param id Especifica id do gestor
+		 * @param password Especifica password do gestor
+		 * @param enderecoEmail Especifica email do gestor
+		 * @return void
+		 */
 	Gestor(std::string nome,unsigned int id,std::string password,std::string enderecoEmail); //ler gestor do ficheiro
+	/**
+		 * @brief Construtor do gestor
+		 *
+		 *	Email e atribuido automaticamente
+		 *
+		 * @param nome Especifica nome do gestor
+		 * @param password Especifica password do gestor
+		 * @param siglaAssociacao Especifica associacao do gestor
+		 * @param id Especifica id do gestor
+		 * @return void
+		 */
 	Gestor(std::string nome, std::string password, std::string siglaAssociacao,unsigned int id); //criar gestor de raiz
 
 	//metodos set
+	/**
+	 * @brief Atribui valor do argumento ao atributo nome
+	 * @param nome Valor que será atribuído ao atributo nome
+	 * @return void
+	 */
 	void setNome(std::string nome);
+	/**
+	 * @brief Atribui valor do argumento ao atributo id
+	 * @param id Valor que será atribuído ao atributo id
+	 * @return void
+	 */
 	void setID (unsigned int id);
+	/**
+	 * @brief Atribui valor do argumento ao atributo password
+	 * @param password Valor que será atribuído ao atributo password
+	 * @return void
+	 */
 	void setPassword(std::string password);
+	/**
+	 * @brief Atribui valor do argumento ao atributo enderecoEmail
+	 * @param enderecoEmail Valor que será atribuído ao atributo enderecoEmail
+	 * @return void
+	 */
 	void setEnderecoEmail(std::string enderecoEmail);
 
 	//metodos get
+	/**
+	 * @brief Retorna valor do atributo nome
+	 * @return Valor do atributo nome
+	 */
 	std::string getNome() const;
+	/**
+	 * @brief Retorna valor do atributo id
+	 * @return Valor do atributo id
+	 */
 	unsigned int getID() const;
+	/**
+	 * @brief Retorna valor do atributo enderecoEmail
+	 * @return Valor do atributo enderecoEmail
+	 */
 	std::string getEmail() const {return this->enderecoEmail;}
+	/**
+	 * @brief Retorna valor do atributo password
+	 * @return Valor do atributo password
+	 */
 	std::string getPassword() const;
+	/**
+	 * @brief Retorna valor do atributo enderecoEmail
+	 * @return Valor do atributo enderecoEmail
+	 */
 	std::string getEnderecoEmail() const;
+	/**
+	 * @brief Retorna valor do atributo emailsRecebidos
+	 * @return Valor do atributo emailsRecebidos
+	 */
 	std::vector<Email*> getEmailsRecebidos();
+	/**
+	 * @brief Retorna valor do atributo emailsEnviados
+	 * @return Valor do atributo emailsEnviados
+	 */
 	std::vector<Email*> getEmailsEnviados();
 
+	/**
+	 * @brief Adiciona um email ao atributo emailsEnviados
+	 * @param email Especifica email a adicionar
+	 * @return void
+	 */
 	void enviarEmail(Email* email);
+	/**
+	 * @brief Adiciona um email ao atributo emailsRecebidos
+	 * @param email Especifica email a adicionar
+	 * @return void
+	 */
 	void receberEmail(Email* email);
 
 	//show
+	/**
+	 * @brief Imprime na consola os atributos do gestor
+	 * @return void
+	 */
 	void show() const;
 
 
